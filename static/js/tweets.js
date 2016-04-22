7fa2491fec10679b76094364dc683a8fdd395299
lily healey <lily@lilyhealey.co.uk>
2016-04-22 15:40:53 -0400

    add keyboard nav + images

--- static/js/tweets.js
+++ static/js/tweets.js
@@ -11,11 +11,41 @@ function show_next_tweet()
     show(tweets[index]);
 }
 
+function show_prev_tweet()
+{
+    // is it bad to increment index here?
+    // probs too easy to miss?
+    hide(tweets[index--]);
+    
+    // reset to the first tweet
+    if (index == -1)
+        index = tweets.length-1;
+
+    show(tweets[index]);
+}
+
+function toggle_rotation()
+{
+    if(tweet_interval)
+    {
+        clearInterval(tweet_interval);
+        tweet_interval = null;
+    }
+    else
+    {
+        show_next_tweet();
+        tweet_interval = setInterval(show_next_tweet, tweet_duration);
+    }
+}
+
 document.onkeydown = function(e) {
     e = e || window.event;
     switch(e.which || e.keyCode) {
+        case 32: // spacebar
+            toggle_rotation();
+        break;
         case 37: // left
-            show_next_tweet();
+            show_prev_tweet();
         break;
         case 39: // right
             show_next_tweet();
--- views/tweets.php
+++ views/tweets.php
@@ -9,24 +9,34 @@ $data = $tweeter->get_tweets($stream_id);
 $tweets = $data->objects->tweets;
 $users = $data->objects->users;
 
+$tco_pattern = '/https:\/\/t\.co\/.*/';
+$apos_pattern = "/(\w+)'(\w+)/";
+
 ?><section id="tweets"><?
     $show = true;
     foreach($tweets as $t)
     {
         $text = $t->text;
+        // remove t.co links
+        $text = preg_replace($tco_pattern, '', $text);
+        // replace dumb apostrophes with smart ones
+        $text = preg_replace($apos_pattern, "$1’$2", $text);
+        
         $dt = $t->created_at;
         $dt = strtotime($dt);
         $date = date($date_fmt, $dt);
         $time = date($time_fmt, $dt);
         $ts = $date." at ".$time;
+        
         $media = $t->entities->media[0];
+        
         if($show)
         {
-        ?><figure class="tweet"><?
+        ?><figure class="animated fadeIn tweet"><?
         }
         else
         {
-        ?><figure class="tweet hidden"><?
+        ?><figure class="animated fadeIn tweet hidden"><?
         }
             ?><div class="text"><? 
                 echo $text; 
@@ -35,6 +45,16 @@ $users = $data->objects->users;
                 echo $ts;
             ?></div>
         </figure><?
+        
+        if($media)
+        {
+        ?><figure class="animated fadeIn tweet hidden">
+            <div class="media">
+                <img src="<? echo $media->media_url; ?>">
+            </div>
+        </figure>
+        <?
+        }
         $show = false;
     }
 ?></section>
@@ -42,7 +62,8 @@ $users = $data->objects->users;
 <script type="text/javascript">
     var tweets = document.getElementsByClassName("tweet");
     var index = 0;
-    
-    var x;
-    x = setInterval(show_next_tweet, 20000);
+    var tweet_duration = 6000;
+    var tweet_interval;
+
+    tweet_interval = setInterval(show_next_tweet, tweet_duration);
 </script>
